5934ae19259ed0307a23428ee4426df20afbad9e
dongkeon-lee <veha357@naver.com>
2022-07-13 15:12:36 +0900

    2022/07/13 15:12 이동건 간략마무리

--- app.py
+++ app.py
@@ -8,33 +8,6 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.fciykbx.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
-# @app.route("/movie", methods=["POST"])
-# def movie_post():
-#     url_receive = request.form['url_give']
-#     star_receive = request.form['star_give']
-#     comment_receive = request.form['comment_give']
-#
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-#     data = requests.get(url_receive, headers=headers)
-#
-#     soup = BeautifulSoup(data.text, 'html.parser')
-#
-#     title = soup.select_one('meta[property="og:title"]')['content']
-#     image = soup.select_one('meta[property="og:image"]')['content']
-#     desc = soup.select_one('meta[property="og:description"]')['content']
-#
-#     doc = {
-#         'title':title,
-#         'image':image,
-#         'desc':desc,
-#         'star':star_receive,
-#         'comment':comment_receive
-#     }
-#     db.movies.insert_one(doc)
-#
-#     return jsonify({'msg':'저장 완료!'})
-
 @app.route("/")
 def movie_get():
     movie_list = list(db.mumu_movie.find({}, {'_id': False}))
